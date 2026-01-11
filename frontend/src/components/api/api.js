ccd7c059b00b0d89f5d0cd7c98691ec5959cf3b6
Meraj-1 <belalraza158@gmail.com>
2026-01-11 20:01:06 +0530

    edit config

--- frontend/src/components/api/api.js
+++ frontend/src/components/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: ["http://localhost:8000/api", "https://focus-board-amber.vercel.app/"],
+  baseURL: ["http://localhost:8000/api", "https://focus-board-amber.vercel.app/api"],
 });
 
 api.interceptors.request.use((config) => {
