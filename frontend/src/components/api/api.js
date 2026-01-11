7faa1de2cbfb1581f18a871dd8bb3be1100d7a25
Meraj-1 <belalraza158@gmail.com>
2026-01-11 19:40:43 +0530

    edit config

--- frontend/src/components/api/api.js
+++ frontend/src/components/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: ["http://localhost:8000/api", "https://vercel.com/meraj-1s-projects/focus-board"],
 });
 
 api.interceptors.request.use((config) => {
